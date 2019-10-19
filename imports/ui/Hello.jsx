83e45967b358aa91de5ec182465f9a757874b063
leeroywking <leeroywking@gmail.com>
2019-10-19 14:28:30 -0700

    cleaned up the tech debt

--- imports/ui/Hello.jsx
+++ imports/ui/Hello.jsx
@@ -12,29 +12,28 @@ class Hello extends Component {
     }
   }
 
-  increment() {
+  increment(id) {
     this.setState({
       counter: this.state.counter + 1
     });
-    Counter.update({ "_id": "hcLFwHfsiHH88uhfQ" }, { "count": this.props.counterArray[0].count + 1 })
+    Counter.update({ "_id": id }, { "count": this.props.counterArray[0].count + 1 })
   }
 
-  decrement() {
+  decrement(id) {
     this.setState({
       counter: this.state.counter - 1
     });
-    Counter.update({ "_id": "hcLFwHfsiHH88uhfQ" }, { "count": this.props.counterArray[0].count - 1 })
+    Counter.update({ "_id": id }, { "count": this.props.counterArray[0].count - 1 })
   }
   render() {
     const visualCount = this.props.counterArray.map(
       countObj => countObj.count
     )
-    console.log(visualCount)
     return (
       <div>
-        <button onClick={() => this.increment()}>Increment</button>
+        <button onClick={() => this.increment(this.props.id[0]._id)}>Increment</button>
         <p>Counter is at {visualCount[0]}.</p>
-        <button onClick={() => this.decrement()}>Decrement</button>
+        <button onClick={() => this.decrement(this.props.id[0]._id)}>Decrement</button>
       </div>
     );
   }
@@ -43,7 +42,8 @@ class Hello extends Component {
 
 CountContainer = withTracker(() => {
   return {
-    counterArray: Counter.find().fetch()
+    counterArray: Counter.find().fetch(),
+    id: Counter.find().fetch()
   };
 })(Hello);
 
