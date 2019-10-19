35cfda77030f6eb9b78f5e7ff6f853c2324e77d2
leeroywking <leeroywking@gmail.com>
2019-10-19 16:05:49 -0700

    Changed title

--- server/main.js
+++ server/main.js
@@ -15,27 +15,6 @@ function updateCounter(count){
 
 Meteor.startup(() => {
   // If the Links collection is empty, add some data.
-  if (Links.find().count() === 0) {
-    insertLink(
-      'Do the Tutorial',
-      'https://www.meteor.com/tutorials/react/creating-an-app'
-    );
-
-    insertLink(
-      'Follow the Guide',
-      'http://guide.meteor.com'
-    );
-
-    insertLink(
-      'Read the Docs',
-      'https://docs.meteor.com'
-    );
-
-    insertLink(
-      'Discussions',
-      'https://forums.meteor.com'
-    );
-  }
 
   if (Counter.find().count() === 0 ){
     updateCounter(0)
